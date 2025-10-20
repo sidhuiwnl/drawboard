4d11523183c6f492bbdf2e0415a335c1c245ff42
Sidharth <sidharthinfernal@gmail.com>
2025-10-20 17:00:40 +0530

    added canvas

--- frontend/components/DrawingCanvas.tsx
+++ frontend/components/DrawingCanvas.tsx
@@ -1,9 +1,10 @@
-import {useEffect, useRef} from "react";
+import {useEffect, useRef,useState} from "react";
 
 export default function DrawingCanvas(){
 
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const contextRef = useRef<CanvasRenderingContext2D>(null);
+    const[isDrawing, setIsDrawing] = useState(false);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -22,11 +23,55 @@ export default function DrawingCanvas(){
         contextRef.current = context;
 
     }, []);
+
+    const startDrawing = (event : React.MouseEvent<HTMLCanvasElement>) => {
+        const { offsetY, offsetX } = event.nativeEvent;
+        setIsDrawing(true);
+
+        if(!contextRef.current){
+            return;
+        }
+
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+    }
+
+    const draw = (event : React.MouseEvent<HTMLCanvasElement>) => {
+        const { offsetX, offsetY } = event.nativeEvent;
+
+        if(!contextRef.current){
+            return;
+        }
+
+        if(!isDrawing){
+            return;
+        }
+
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke();
+    }
+    const endDrawing = (event : React.MouseEvent<HTMLCanvasElement>) => {
+        if(!contextRef.current){
+            return;
+        }
+        setIsDrawing(false);
+        contextRef.current.closePath();
+    }
+
+    console.log("Rendering canvas");
+
     return (
         <div>
             <canvas
+                className="w-screen h-screen"
                 ref={canvasRef}
-
+                onMouseDown={startDrawing}
+                onMouseUp={endDrawing}
+                onMouseLeave={endDrawing}
+                onMouseMove={draw}
+                style={{
+                    cursor: 'crosshair',
+                }}
             />
         </div>
     )

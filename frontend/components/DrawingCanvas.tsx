f4ef5f4f7554610e37371359276c5e4fb1704097
Sidharth <sidharthinfernal@gmail.com>
2025-10-20 18:20:24 +0530

    undo functionality

--- frontend/components/DrawingCanvas.tsx
+++ frontend/components/DrawingCanvas.tsx
@@ -1,10 +1,16 @@
 import {useEffect, useRef,useState} from "react";
 
+
+
+
 export default function DrawingCanvas(){
 
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const contextRef = useRef<CanvasRenderingContext2D>(null);
     const[isDrawing, setIsDrawing] = useState(false);
+    const[undoState, setUndoState] = useState<ImageData[]>([]);
+
+    console.log(undoState);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -22,10 +28,29 @@ export default function DrawingCanvas(){
         context.lineWidth = 5;
         contextRef.current = context;
 
+        saveState()
+
     }, []);
+
+    const saveState = () => {
+        if (!canvasRef.current || !contextRef.current) return;
+
+        const imageData = contextRef.current.getImageData(
+            0,0,
+            canvasRef.current.width,
+            canvasRef.current.height,
+        )
+        setUndoState(prev => [...prev, imageData]);
+
+
+    }
 
     const startDrawing = (event : React.MouseEvent<HTMLCanvasElement>) => {
         const { offsetY, offsetX } = event.nativeEvent;
+
+        saveState();
+
+
         setIsDrawing(true);
 
         if(!contextRef.current){
@@ -47,30 +72,62 @@ export default function DrawingCanvas(){
             return;
         }
 
+
+
         contextRef.current.lineTo(offsetX, offsetY);
         contextRef.current.stroke();
     }
-    const endDrawing = (event : React.MouseEvent<HTMLCanvasElement>) => {
+    const endDrawing = () => {
         if(!contextRef.current){
             return;
         }
+
+
         setIsDrawing(false);
         contextRef.current.closePath();
     }
 
-    console.log("Rendering canvas");
+   const undo = () => {
+       if (!contextRef.current || !canvasRef.current) {
+           return;
+       }
+
+
+        if(undoState.length <= 2 ){
+            return;
+        }
+
+        const newStack = undoState.slice(0,-1);
+
+       const previousState = newStack[newStack.length - 1];
+
+
+        contextRef.current.putImageData(previousState,0,0)
+
+       setUndoState(newStack);
+   }
+
 
     return (
         <div>
+            <button
+                className="btn btn-primary"
+                onClick={() => undo()}
+            >
+                Undo
+            </button>
+
             <canvas
                 className="w-screen h-screen"
-                ref={canvasRef}
+                    ref={canvasRef}
                 onMouseDown={startDrawing}
                 onMouseUp={endDrawing}
                 onMouseLeave={endDrawing}
                 onMouseMove={draw}
+
                 style={{
                     cursor: 'crosshair',
+                    background: "black",
                 }}
             />
         </div>
